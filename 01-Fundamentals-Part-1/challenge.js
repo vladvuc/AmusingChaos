54eeb4be44d39ea005a3f4c1a2137926bff96ae0
Vladimir Vučković <oliver@thewayisee.net>
2023-06-18 16:28:56 +0200

    Playing with JS

--- 01-Fundamentals-Part-1/challenge.js
+++ 01-Fundamentals-Part-1/challenge.js
@@ -1,4 +1,4 @@
-// Cooding challenge 4
+// Cooding challenge # 1
 /* Write your code below. Good luck! 🙂 */
 
 const massMark = 95
@@ -21,3 +21,34 @@ if (BMIMark > BMIJohn) {
 console.log(markHigherBMI)
 
 console.log(BMIMark, BMIJohn, markHigherBMI)
+
+// Cooding challenge # 2
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+/* Write your code below. Good luck! 🙂 */
+
+// 1. Print a nice output to the console, telling the user who has the higher BMI. 
+// The message can be either:
+if (BMIMark > BMIJohn) {
+    console.log("Mark's BMI is higher than John's!")
+}
+else {
+    console.log("John's BMI is higher than Mark's")
+}
+
+// 2. Modify the outputs above to use template literals 
+// to include the BMI values in the outputs.
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
+}
--- 01-Fundamentals-Part-1/from_class.js
+++ 01-Fundamentals-Part-1/from_class.js
@@ -10,3 +10,43 @@
 // Conditonal variable
 const birthYear = 1991;
 
+let century;
+
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century);
+
+// Cooding challenge # 2
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+/* Write your code below. Good luck! 🙂 */
+
+// 1. Print a nice output to the console, telling the user who has the higher BMI. 
+// The message can be either:
+if (BMIMark > BMIJohn) {
+    console.log("Mark's BMI is higher than John's!")
+}
+else {
+    console.log("John's BMI is higher than Mark's")
+}
+
+// 2. Modify the outputs above to use template literals 
+// to include the BMI values in the outputs.
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
+}
