0930ba5f80336ca5eb3d2da1d57942a00a7c5565
Vladimir Vučković <oliver@thewayisee.net>
2023-06-19 16:04:26 +0200

    Playing with strings and numbers

--- 01-Fundamentals-Part-1/final_script.js
+++ 01-Fundamentals-Part-1/final_script.js
@@ -1,57 +1,127 @@
-console.log('=== Main script ===')
-/*
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// // JAVASCRIPT BASICS CALCULATIONS
 
-// Value is a piece of data
-console.log('Jonas');
-console.log(23);
-console.log('This works?') // apparently it does
+// console.log('=== Main script ===')
+// /*
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
 
-let firstName = 'Vladimir';
-console.log('The Console log now is ' + firstName);
-console.log(firstName);
-console.log(firstName);
+// // Value is a piece of data
+// console.log('Jonas');
+// console.log(23);
+// console.log('This works?') // apparently it does
 
-let myFirstJob = 'Tour guide'
-let counter = true;
-console.log(counter);
-console.log(typeof counter);
+// let firstName = 'Vladimir';
+// console.log('The Console log now is ' + firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-*/
-const now = 2022;
-const ageJonas = now - 1990;
-const ageSarah = now - 2018;
-console.log(now - 1991 > now - 2018);
+// let myFirstJob = 'Tour guide'
+// let counter = true;
+// console.log(counter);
+// console.log(typeof counter);
 
-let x, y;
-x = y = 25 - 10 - 5 // x= y = 10
-console.log(x, y);
+// */
+// const now = 2022;
+// const ageJonas = now - 1990;
+// const ageSarah = now - 2018;
+// console.log(now - 1991 > now - 2018);
 
-const averageAge = (ageJonas + ageSarah) / 2
-console.log('Jonas ', ageJonas, 'Sarah ', ageSarah);
-console.log(averageAge)
+// let x, y;
+// x = y = 25 - 10 - 5 // x= y = 10
+// console.log(x, y);
 
-// Template String and Strings in General
+// const averageAge = (ageJonas + ageSarah) / 2
+// console.log('Jonas ', ageJonas, 'Sarah ', ageSarah);
+// console.log(averageAge)
 
-const firstName = 'Vlad';
-const job = 'student of AI';
-const birthYear = 1990;
-const currentYear = 2023;
+// // STRINGS AND TEMPLATE STRINGS
 
-const vlad = "I'm " + firstName + ', a ' + (currentYear - birthYear) + ' years old ' + job + '!';
+// const firstName = 'Vlad';
+// const job = 'student of AI';
+// const birthYear = 1990;
+// const currentYear = 2023;
 
-console.log(vlad)
+// const vlad = "I'm " + firstName + ', a ' + (currentYear - birthYear) + ' years old ' + job + '!';
 
-const vladNew = `I'm ${firstName}, a ${currentYear - birthYear} year old ${job}!`;
-console.log(vladNew)
+// console.log(vlad)
 
-console.log(`This is just a regular string... It is much easier because you don't need to use the quotes or single quoatitions.`)
+// const vladNew = `I'm ${firstName}, a ${currentYear - birthYear} year old ${job}!`;
+// console.log(vladNew)
 
-// Multiline string
+// console.log(`This is just a regular string... It is much easier because you don't need to use the quotes or single quoatitions.`)
 
-console.log(`This is a string
-And this is a new line of a string.
-And this is a new new line of a string, so we
-can write paragraphs like this without thinking
-too much about like \n or some other bulshit.`)
+// // Multiline string
+
+// console.log(`This is a string
+// And this is a new line of a string.
+// And this is a new new line of a string, so we
+// can write paragraphs like this without thinking
+// too much about like \n or some other bulshit.`)
+
+// // const age = 15;
+
+// // if (age >= 18) {
+// //     console.log('The person can start driving lessons 🚗 ');
+// // } else {
+// //     const yearsLeft = 18 - age;
+// //     console.log(`This person is too young ⛔️. Wait another ${yearsLeft} years.`);
+// // }
+
+// // CONDITIONAL VARIABLE
+// const birthYear = 1991;
+
+// let century;
+
+// if (birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
+
+// console.log(century);
+
+// // COODING CHALLENGE # 2
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+// /* Write your code below. Good luck! 🙂 */
+
+// // 1. Print a nice output to the console, telling the user who has the higher BMI. 
+// // The message can be either:
+// if (BMIMark > BMIJohn) {
+//     console.log("Mark's BMI is higher than John's!")
+// }
+// else {
+//     console.log("John's BMI is higher than Mark's")
+// }
+
+// // 2. Modify the outputs above to use template literals 
+// // to include the BMI values in the outputs.
+
+// if (BMIMark > BMIJohn) {
+//     console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+// } else {
+//     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
+// }
+
+// // TYPE CONVERSION
+// const inputYear = '1991';
+// console.log(Number(inputYear), inputYear)
+// console.log(Number(inputYear) + 18);
+// console.log(Number('Jonas'));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// // type coercion
+// console.log('I am ' + 23 + ' years old');
+// console.log('I am ' + String(23) + ' years old');
+
+// console.log('23' - '10' - '3')
--- 01-Fundamentals-Part-1/from_class.js
+++ 01-Fundamentals-Part-1/from_class.js
@@ -1,52 +1,14 @@
-// const age = 15;
+// TYPE CONVERSION
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear)
+console.log(Number(inputYear) + 18);
+console.log(Number('Jonas'));
+console.log(typeof NaN);
 
-// if (age >= 18) {
-//     console.log('The person can start driving lessons 🚗 ');
-// } else {
-//     const yearsLeft = 18 - age;
-//     console.log(`This person is too young ⛔️. Wait another ${yearsLeft} years.`);
-// }
+console.log(String(23), 23);
 
-// Conditonal variable
-const birthYear = 1991;
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('I am ' + String(23) + ' years old');
 
-let century;
-
-if (birthYear <= 2000) {
-    century = 20;
-} else {
-    century = 21;
-}
-
-console.log(century);
-
-// Cooding challenge # 2
-
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
-
-const BMIMark = massMark / (heightMark * heightMark);
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-console.log(BMIMark, BMIJohn);
-
-/* Write your code below. Good luck! 🙂 */
-
-// 1. Print a nice output to the console, telling the user who has the higher BMI. 
-// The message can be either:
-if (BMIMark > BMIJohn) {
-    console.log("Mark's BMI is higher than John's!")
-}
-else {
-    console.log("John's BMI is higher than Mark's")
-}
-
-// 2. Modify the outputs above to use template literals 
-// to include the BMI values in the outputs.
-
-if (BMIMark > BMIJohn) {
-    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
-} else {
-    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
-}
+console.log('23' - '10' - '3')
